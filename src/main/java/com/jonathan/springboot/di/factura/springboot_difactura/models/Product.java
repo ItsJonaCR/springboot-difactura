6982988d2f50ec9ec5d28bfb5b8c82c60692b7ac
ItsJonaCR <jonavasquez.cr@gmail.com>
2025-10-16 10:05:15 -0600

    first commit

---
+++ src/main/java/com/jonathan/springboot/di/factura/springboot_difactura/models/Product.java
@@ -0,0 +1,24 @@
+package com.jonathan.springboot.di.factura.springboot_difactura.models;
+
+public class Product {
+
+    private String name;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}
